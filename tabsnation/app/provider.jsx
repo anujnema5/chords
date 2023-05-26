250adb27fb2805c432b27e3a8247445e84dc57b8
Anuj nema <anujnema63@gmail.com>
2023-05-26 15:41:14 +0530

    fix-4

--- tabsnation/app/provider.jsx
+++ tabsnation/app/provider.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 const Provider = ({children}) => {
     return (
-        <ThemeProvider attribute='class' defaultTheme='light'>
+        <ThemeProvider attribute='class' defaultTheme='dark'>
             {children}
         </ThemeProvider>
     )
